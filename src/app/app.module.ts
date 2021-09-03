698750d739ad1ff4bdb2a8524cc2681d6cf10fc2
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-09-03 18:43:20 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FirstpageComponent } from './navigation/firstpage/firstpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent,
+    FirstpageComponent
   ],
   imports: [
     BrowserModule,
